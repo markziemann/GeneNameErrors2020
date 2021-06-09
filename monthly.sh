f68814c49cefbcea2037ed337d4b4c5717451298
markziemann <markziemann>
2021-06-09 10:28:32 +1000

    fixed monthly scheduler issue with env vars

--- monthly.sh
+++ monthly.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-
+set -x
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin
 # run the monthly report
 
 # here is the cron job for 3rd day of the month
-#0 0 3 * * bash /mnt/mziemann/projects/GeneNameErrors2020/monthly.sh
+#0 0 3 * * /bin/bash /home/mdz/projects/GeneNameErrors2020/monthly.sh >> /home/mdz/projects/GeneNameErrors2020/cron.log 2>&1
 
 # because it is cron scheduled it needs to cd to the directory where the files are located
-cd "$(dirname "$0")"
+#cd "$(dirname "$0")"
+
+cd /home/mdz/projects/GeneNameErrors2020
+
+date >> monthly_start.txt
 
 R -e rmarkdown::render"('monthly.Rmd',output_file='monthly.html')"
 
